885a7da34603b460f87e4eac1907f5e60af0b758
ryunaja <Kawiwat.ryu@gmail.com>
2025-02-20 15:11:56 +0700

    add multiple versions support and more interaction with pip
    Now be able to select what python versions to use.
    Show description for each package.

--- pipStuff.py
+++ pipStuff.py
@@ -1,27 +1,67 @@
 import subprocess
 from dataclasses import dataclass
 
-@dataclass
-class info:
-    name: str
-    version: str
-    summary: str
 
-def get_installed() -> list[tuple[str, str]]:
-    results = subprocess.run(["pip", "list"], capture_output=True)
+@dataclass()
+class Version:
+    major: int
+    minor: int
+
+    def __repr__(self):
+        return f"{self.major}.{self.minor}"
+
+    def __str__(self):
+        return f"{self.major}.{self.minor}"
+
+
+@dataclass()
+class Info:
+    name: str | None
+    version: str | None
+    summary: str | None = None
+    homepage: str | None = None
+    author: str | None = None
+    author_email: str | None = None
+    license: str | None = None
+    location: str | None = None
+    requires: str | None = None
+    required_by: str | None = None
+
+
+def get_installed(python_version: Version | None=None) -> list[tuple[str, str]]:
+    pip_arg = ["python", "-m", "pip", "list"]
+    if python_version != None:
+        pip_arg[0] = f"python{python_version}"
+    results = subprocess.run(pip_arg, capture_output=True)
     results = results.stdout.decode('utf-8')
     results = results.splitlines()
-    results.pop(0)
-    results.pop(0)
+    results = results[2::]
     out = []
     for result in results:
-        out.append(result.split())
+        out.append(Info(result.split()[0], result.split()[1]))
     return out
 
 
-def install_pip(package: str) -> subprocess.CompletedProcess:
-    return subprocess.run(["pip", "install", package], capture_output=True)
+def install_pip(package: str, python_version: Version | None=None) -> subprocess.CompletedProcess:
+    pip_arg = ["python", "-m", "pip", "install", package]
+    if python_version != None:
+        pip_arg[0] = f"python{python_version}"
+    return subprocess.run(pip_arg, capture_output=True)
 
 
-def get_package_info(package: str):
-    pass
+def get_package_info(package: str, python_version: Version | None=None) -> Info:
+    pip_arg = ["python", "-m", "pip", "show", package]
+    if python_version != None:
+        pip_arg[0] = f"python{python_version}"
+    results = subprocess.run(pip_arg, capture_output=True)
+    results = results.stdout.decode('utf-8')
+    results = results.splitlines()
+    results = [(result.split()[1] if len(result.split()) > 1 else None) for result in results]
+    return Info(results[0], results[1], results[2], results[3], results[4], results[5], results[6], results[7], results[8], results[9])
+
+
+def ensure_pip(package: str, python_version: Version | None=None) -> subprocess.CompletedProcess:
+    pip_arg = ["python", "-m", "ensurepip", "install", "--upgrade"]
+    if python_version != None:
+        pip_arg[0] = f"python{python_version}"
+    return subprocess.run(pip_arg, capture_output=True)
