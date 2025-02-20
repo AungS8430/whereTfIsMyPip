87098c5221087b0c9899f253c9df22c6982d7b82
aungs8430 <aungshome@outlook.com>
2025-02-21 06:20:46 +0700

    Added package search feature

--- getPackage.py
+++ getPackage.py
@@ -2,7 +2,11 @@ import requests
 
 def get():
     r = requests.get("https://pypi.org/simple/")
-    packages = r.content.split("\n")
-    print(packages)
+    packages = str(r.content).split("\\n")[2:-2]
+    for i in range(len(packages)):
+        packages[i] = packages[i].split(">")[1].split("<")[0]
+    return packages
 
-get()
+def search(packages, query):
+    result = [package for package in packages if package[0:len(query)] == query]
+    return result
