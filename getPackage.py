edb611c2d5b53471dcfd43a389f23dddbd8f39f7
aungs8430 <aungshome@outlook.com>
2025-02-21 20:11:05 +0700

    Added package info backend.

--- getPackage.py
+++ getPackage.py
@@ -1,4 +1,38 @@
+import dataclasses
+from dataclasses import dataclass
 import requests
+import inspect
+
+@dataclass()
+class pkgUrls:
+    Documentation: str | None
+    Homepage: str | None
+    Source: str | None
+
+@dataclass()
+class pkgInfo:
+    author: str | None
+    author_email: str | None
+    bugtrack_url: str | None
+    classifiers: list | None
+    description: str | None
+    description_content_type: str | None
+    docs_url: str | None
+    home_page: str | None
+    license: str | None
+    name: str | None
+    package_url: str | None
+    project_url: pkgUrls | None
+    requires_dist: list | None
+    requires_python: str | None
+    summary: str | None
+    version: str | None
+    @classmethod
+    def from_dict(cls, env):      
+        return cls(**{
+            k: v for k, v in env.items() 
+            if k in inspect.signature(cls).parameters
+        })
 
 def get():
     r = requests.get("https://pypi.org/simple/")
@@ -10,3 +44,8 @@ def get():
 def search(packages, query):
     result = [package for package in packages if package[0:len(query)] == query]
     return result
+
+def getInfo(packages, query):
+    if query not in packages:
+        return 1
+    result = pkgInfo.from_dict(requests.get(f"https://pypi.org/pypi/{query}/json").json()["info"])
