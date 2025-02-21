1c44c95f54c6a905544167c225091cf4bcab7fab
aungs8430 <aungshome@outlook.com>
2025-02-21 21:50:47 +0700

    Moved to EEL for GUI

--- main.py
+++ main.py
@@ -1,14 +1,5 @@
-import sys
-from PySide6.QtGui import QGuiApplication
-from PySide6.QtQml import QQmlApplicationEngine
+import eel
 
 if __name__ == "__main__":
-    app = QGuiApplication(sys.argv)
-    engine = QQmlApplicationEngine()
-    engine.addImportPath(sys.path[0])
-    engine.loadFromModule("App", "Main")
-    if not engine.rootObjects():
-        sys.exit(-1)
-    exit_code = app.exec()
-    del engine
-    sys.exit(exit_code)
+    eel.init("gui")
+    eel.start("index.html", size=(100, 100))
